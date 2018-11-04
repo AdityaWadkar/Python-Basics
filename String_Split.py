e2eaa3b58de706ae62a0ec21803a762440a05c44
Parthi05 <44748171+Parthi05@users.noreply.github.com>
2018-11-04 19:23:46 +0530

    Add files via upload

---
+++ String_Split.py
@@ -0,0 +1,13 @@
+from __future__ import print_function
+
+# 1 --- Split #
+print('*'*10 + '1. Split ' + '*'*10 '\n')
+ip_addr = input("Enter an IP address: ")
+print(ip_addr.split("."))
+print('\n')
+
+# 2 --- Split sentence White space #
+print('*'*10 + '2. Split White space' + '*'*10 '\n')
+sentence = input("Enter a sentence: ")
+print(sentence.split())
+print('\n')
